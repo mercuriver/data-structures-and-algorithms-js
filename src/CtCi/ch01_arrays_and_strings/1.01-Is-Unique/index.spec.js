873bc040942315300d18e3c1422e1d09fe0f013a
Mercuriver <mercuriver@gmail.com>
2022-08-01 22:16:57 +0900

    refactor(ch01): 중복 체크 테스트 케이스 코드 개선

--- src/CtCi/ch01_arrays_and_strings/1.01-Is-Unique/index.spec.js
+++ src/CtCi/ch01_arrays_and_strings/1.01-Is-Unique/index.spec.js
@@ -1,39 +1,27 @@
 import assert from "assert";
 import isUnique from "./index.js";
 
-const case1 = ["abcd", true];
-const case2 = ["abccd", false];
-const case3 = ["bhjjb", false];
-const case4 = ["mdjq", true];
+const data = [
+  ["abcd", true],
+  ["abccd", false],
+  ["mercuriver", false],
+  ["string", true],
+];
 
 describe("중복이 없는가", function () {
   describe("기본적인 풀이", function () {
-    it(`${case1[0]}는 ${case1[1]}이여야 함`, function () {
-      assert.equal(isUnique(case1[0]), case1[1]);
-    });
-    it(`${case2[0]}는 ${case2[1]}이여야 함`, function () {
-      assert.equal(isUnique(case2[0]), case2[1]);
-    });
-    it(`${case3[0]}는 ${case3[1]}이여야 함`, function () {
-      assert.equal(isUnique(case3[0]), case3[1]);
-    });
-    it(`${case4[0]}는 ${case4[1]}이여야 함`, function () {
-      assert.equal(isUnique(case4[0]), case4[1]);
+    data.forEach((datum) => {
+      it(`${datum[0]}는 ${datum[1]}이여야 함`, function () {
+        assert.equal(isUnique(datum[0]), datum[1]);
+      });
     });
   });
 
   describe("추가 자료구조를 사용하지 않는 풀이", function () {
-    // it(`${case1[0]}는 ${case1[1]}이여야 함`, function () {
-    //   assert.equal(isUnique(case1[0]), case1[1]);
-    // });
-    // it(`${case2[0]}는 ${case2[1]}이여야 함`, function () {
-    //   assert.equal(isUnique(case2[0]), case2[1]);
-    // });
-    // it(`${case3[0]}는 ${case3[1]}이여야 함`, function () {
-    //   assert.equal(isUnique(case3[0]), case3[1]);
-    // });
-    // it(`${case4[0]}는 ${case4[1]}이여야 함`, function () {
-    //   assert.equal(isUnique(case4[0]), case4[1]);
-    // });
+    data.forEach((datum) => {
+      it(`${datum[0]}는 ${datum[1]}이여야 함`, function () {
+        assert.equal(isUnique(datum[0]), datum[1]);
+      });
+    });
   });
 });
