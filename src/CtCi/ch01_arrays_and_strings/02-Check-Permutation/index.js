5163df5f51188a1e39e3ee0267c853014e608341
Mercuriver <mercuriver@gmail.com>
2022-08-03 00:05:17 +0900

    feat(ch01): q2 #1, 문자 배열 => count map 처리

--- src/CtCi/ch01_arrays_and_strings/02-Check-Permutation/index.js
+++ src/CtCi/ch01_arrays_and_strings/02-Check-Permutation/index.js
@@ -1,4 +1,18 @@
 const checkPermute = (firstValue, secondValue) => {
+  const charMap = new Map();
+
+  console.log(firstValue, secondValue);
+
+  for (const char of firstValue) {
+    if (!charMap.has(char)) {
+      charMap.set(char, 1);
+    } else {
+      charMap.set(char, charMap.get(char) + 1);
+    }
+  }
+
+  console.log([...charMap]);
+
   return false;
 };
 
--- src/CtCi/ch01_arrays_and_strings/02-Check-Permutation/index.spec.js
+++ src/CtCi/ch01_arrays_and_strings/02-Check-Permutation/index.spec.js
@@ -1,19 +1,23 @@
 import assert from "assert";
 import checkPermute from "./index.js";
+import { stringToCharArray } from "../utils/index.js";
 
 const data = [
   ["abc", "bac", true],
-  ["", "", true],
-  ["1234567812345678", "8877665544332211", true],
-  ["mercuriver", "mercuryriver", false],
-  ["abc", "aabbcc", false],
-  ["12345", "23456", false],
+  // ["", "", true],
+  // ["1234567812345678", "8877665544332211", true],
+  // ["mercuriver", "mercuryriver", false],
+  // ["abc", "aabbcc", false],
+  // ["12345", "23456", false],
 ];
 
 describe("순열 확인", function () {
   data.forEach((datum) => {
     it(`"${datum[0]}", "${datum[1]}"의 순열 여부는 [${datum[2]}]`, function () {
-      assert.equal(checkPermute(datum[0], datum[1]), datum[2]);
+      assert.equal(
+        checkPermute(stringToCharArray(datum[0]), stringToCharArray(datum[1])),
+        datum[2]
+      );
     });
   });
 });
