c03ef022ab08fcbea5c4e466f9f9f28400e6b083
Mercuriver <mercuriver@gmail.com>
2022-08-03 21:13:32 +0900

    test(ch01): q2 테스트 케이스 추가

--- src/CtCi/ch01_arrays_and_strings/02-Check-Permutation/index.spec.js
+++ src/CtCi/ch01_arrays_and_strings/02-Check-Permutation/index.spec.js
@@ -4,11 +4,12 @@ import { stringToCharArray } from "../utils/index.js";
 
 const data = [
   ["abc", "bac", true],
-  ["", "", true],
   ["1234567812345678", "8877665544332211", true],
+  ["", "", false],
   ["mercuriver", "mercuryriver", false],
   ["abc", "aabbcc", false],
   ["12345", "23456", false],
+  ["aabbcc", "abc", false],
 ];
 
 describe("순열 확인", function () {
