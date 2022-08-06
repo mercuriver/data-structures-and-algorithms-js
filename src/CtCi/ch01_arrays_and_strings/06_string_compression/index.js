cee55f600ac1217d9ef1ecc0cc247af53393c250
Mercuriver <mercuriver@gmail.com>
2022-08-07 01:06:09 +0900

    feat(ch01): q6, "문자열 압축" 구현

--- src/CtCi/ch01_arrays_and_strings/06_string_compression/index.js
+++ src/CtCi/ch01_arrays_and_strings/06_string_compression/index.js
@@ -1,5 +1,37 @@
-const stringCompression = () => {
-  return "";
+import { toString } from "../utils/index.js";
+
+const stringCompression = (value) => {
+  const { length } = value;
+
+  if (length <= 2) {
+    return toString(value);
+  }
+
+  const [first] = value;
+  const { prev, result, count } = value.reduce(
+    ({ prev, result, count }, char) => {
+      if (prev !== char) {
+        return {
+          prev: char,
+          result: `${result}${prev}${count}`,
+          count: 1,
+        };
+      }
+
+      return {
+        prev,
+        result,
+        count: count + 1,
+      };
+    },
+    {
+      prev: first,
+      result: "",
+      count: 0,
+    }
+  );
+
+  return result.length < length ? `${result}${prev}${count}` : toString(value);
 };
 
 export default stringCompression;
---
+++ src/CtCi/ch02_linked_lists/02_return_kth_to_last/index.js
@@ -0,0 +1,5 @@
+const findKthToLast = () => {
+  return;
+};
+
+export default findKthToLast;
