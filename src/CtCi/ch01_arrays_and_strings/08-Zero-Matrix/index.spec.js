d682d620f49ff81f44c005edd24e29c1b028736b
mercuriver <mercuriver@gmail.com>
2022-08-06 14:20:17 +0900

    test(ch01): q8 테스트 케이스 작성

---
+++ src/CtCi/ch01_arrays_and_strings/08-Zero-Matrix/index.spec.js
@@ -0,0 +1,72 @@
+import assert from "assert";
+import zeroMatrix, {
+  ERROR_CODE_INVALID_MATRIX,
+  ERROR_CODE_UNFIT_MATRIX,
+} from "./index.js";
+
+const invalidData = [null, undefined, []];
+const unfitMatrixData = [
+  [["10"], ["1"]],
+  [["00", "10"], ["10"]],
+  [
+    ["00", "000", "100"],
+    ["100", "000", "000"],
+  ],
+];
+const data = [
+  [["1"], ["1"]],
+  [["0"], ["0"]],
+  [
+    ["11", "10"],
+    ["10", "00"],
+  ],
+  [
+    ["01", "10"],
+    ["00", "00"],
+  ],
+  [
+    ["11", "11"],
+    ["11", "11"],
+  ],
+  [
+    ["111", "101", "111"],
+    ["101", "000", "101"],
+  ],
+  [
+    ["1111", "1111", "1111", "1110"],
+    ["1110", "1110", "1110", "0000"],
+  ],
+  [
+    ["1111", "1011", "1110", "1111"],
+    ["1010", "0000", "0000", "1010"],
+  ],
+];
+
+// assert가 chai.assert가 아닌 node.assert인 것에 주의
+describe("0 행렬", () => {
+  invalidData.forEach((datum) => {
+    it("값이 [ null | undefined | empty ] 일 경우 에러 발생", () =>
+      assert.throws(
+        () => {
+          zeroMatrix(datum);
+        },
+        (error) => error.message === ERROR_CODE_INVALID_MATRIX
+      ));
+  });
+
+  unfitMatrixData.forEach((datum) => {
+    it(`유효하지 않은 행렬 ${datum}`, () =>
+      assert.throws(
+        () => {
+          zeroMatrix(datum);
+        },
+        (error) => error.message === ERROR_CODE_UNFIT_MATRIX
+      ));
+  });
+
+  data.forEach((datum) => {
+    it(`zeroMatrix("${datum[0]}") => "${datum[1]}"`, () => {
+      assert.equal(zeroMatrix(datum[0]), datum[1]);
+    });
+  });
+});
