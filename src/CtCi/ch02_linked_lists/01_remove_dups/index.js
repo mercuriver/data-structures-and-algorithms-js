3eabd3644360b29476bad64b94b9c443b6c4cd6b
mercuriver <mercuriver@gmail.com>
2022-08-15 01:55:35 +0900

    feat(ch02): ch01. solve

--- src/CtCi/ch02_linked_lists/01_remove_dups/index.js
+++ src/CtCi/ch02_linked_lists/01_remove_dups/index.js
@@ -1,6 +1,18 @@
 const removeDuplicates = (value) => {
-  console.log(value.toArray());
+  const maskMap = new Set();
+  let current = value.head;
+  maskMap.add(current.value);
 
+  while (current.next !== null) {
+    const value = current.next.value;
+    if (maskMap.has(value)) {
+      current.next = current.next.next;
+    } else {
+      maskMap.add(value);
+      current = current.next;
+    }
+  }
   return value;
 };
+
 export default removeDuplicates;
