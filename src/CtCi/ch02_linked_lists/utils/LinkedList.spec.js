ba30b5af3c00bcc49656182c29c77396c10fdbc1
mercuriver <mercuriver@gmail.com>
2022-08-14 12:50:26 +0900

    test(ch02): LinkedList 테스트 케이스 보강

--- src/CtCi/ch02_linked_lists/utils/LinkedList.spec.js
+++ src/CtCi/ch02_linked_lists/utils/LinkedList.spec.js
@@ -1,16 +1,46 @@
 import { expect } from "chai";
 import LinkedList from "./LinkedList.js";
 
-const listArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const listArray = [1, 2, 3, 4];
 
 describe("LinkedList class 테스트", function () {
-  it(`연결 리스트 생성`, function () {
-    const list = new LinkedList();
+  const list = new LinkedList();
 
+  it(`연결 리스트 생성`, function () {
     for (const unit of listArray) {
       list.append(unit);
     }
+    expect(list.toArray()).to.eql(listArray);
+  });
 
-    expect(listArray).to.eql(list.toArray());
+  it(`removeAt(2), 삭제 반환값 3`, function () {
+    expect(list.removeAt(2)).to.equal(3);
+    expect(list.toArray()).to.eql([1, 2, 4]);
+  });
+
+  it(`pop(), 반환값 4`, function () {
+    expect(list.pop()).to.equal(4);
+    expect(list.toArray()).to.eql([1, 2]);
+  });
+
+  it(`insertAt() 테스트`, function () {
+    expect(list.insertAt(1, 4)).to.be.true;
+    expect(list.insertAt(1, 3)).to.be.true;
+    expect(list.insertAt(0, 0)).to.be.true;
+    expect(list.toArray()).to.eql([0, 1, 3, 4, 2]);
+  });
+
+  it(`연속 shift(), pop() 테스트`, function () {
+    expect(list.shift()).to.equal(0);
+    expect(list.pop()).to.equal(2);
+    expect(list.shift()).to.equal(1);
+    expect(list.toArray()).to.eql([3, 4]);
+  });
+
+  it(`연속 prepend() 테스트`, function () {
+    list.prepend(2);
+    list.prepend(1);
+    list.prepend(0);
+    expect(list.toArray()).to.eql([0, 1, 2, 3, 4]);
   });
 });
