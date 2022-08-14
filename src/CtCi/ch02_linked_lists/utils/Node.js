b31b337f40b8d35449b5176174f51fdd9003dca7
mercuriver <mercuriver@gmail.com>
2022-08-15 01:55:51 +0900

    feat(ch02): Node class, Add to string

--- src/CtCi/ch02_linked_lists/utils/Node.js
+++ src/CtCi/ch02_linked_lists/utils/Node.js
@@ -16,6 +16,13 @@ class Node {
   set next(next) {
     this.#next = next;
   }
+
+  toString() {
+    return JSON.stringify({
+      value: this.#value,
+      next: this.#next?.value || undefined,
+    });
+  }
 }
 
 export default Node;
