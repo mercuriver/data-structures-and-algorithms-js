3c78d246ba5a363bf80bb265f7af96e7a6dde451
mercuriver <mercuriver@gmail.com>
2022-08-14 12:07:46 +0900

    feat(ch02): utils class LinkedList, insertAt, RemoveAt 함수 작성

--- src/CtCi/ch02_linked_lists/utils/LinkedList.js
+++ src/CtCi/ch02_linked_lists/utils/LinkedList.js
@@ -57,12 +57,49 @@ class LinkedList {
     return first;
   }
 
-  get head() {
-    return this.#head;
+  removeAt(index) {
+    let i = 0;
+    let current = this.#head;
+    let prev = null;
+
+    while (current !== null) {
+      if (i == index) {
+        if (prev === null) {
+          this.#head = current.next;
+        } else {
+          prev.next = current.next;
+          current.next = null;
+          return current.value;
+        }
+      } else {
+        prev = current;
+        current = current.next;
+        i++;
+      }
+    }
+    return null;
   }
 
-  get tail() {
-    return this.#tail;
+  insertAt(index, value) {
+    if (index === 0) {
+      return this.prepend(value);
+    }
+
+    let current = this.#head;
+    let i = 0;
+
+    while (current !== null) {
+      if (i == index - 1) {
+        current.next = new Node(value, current.next);
+        return true;
+      } else {
+        i++;
+        current = current.next;
+      }
+    }
+
+    // Todo: index가 length를 초과할 경우, 마지막에 append 할지 여부 고민
+    return false;
   }
 
   toArray() {
@@ -73,6 +110,14 @@ class LinkedList {
       current = current.next;
     }
     return result;
+  }
+
+  get head() {
+    return this.#head;
+  }
+
+  get tail() {
+    return this.#tail;
   }
 }
 
