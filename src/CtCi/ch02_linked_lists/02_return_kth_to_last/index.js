ef0937701310c6a5f4a0b4bc5cf0a5ba8f317ce7
Mercuriver <mercuriver@gmail.com>
2022-08-15 20:18:45 +0900

    feat(ch02): ch02, 스택을 사용한 풀이 구현

--- src/CtCi/ch02_linked_lists/02_return_kth_to_last/index.js
+++ src/CtCi/ch02_linked_lists/02_return_kth_to_last/index.js
@@ -1,5 +1,30 @@
-const findKthToLast = () => {
-  return;
+// Todo: 포인터를 사용한 처리 방식
+
+// Todo: 재귀를 사용한 처리 방식
+
+// Stack을 사용한 처리 방식
+const findKthToLast = (value, index) => {
+  let current = value.head;
+  const miniStack = [current.value];
+
+  if (index < 0) {
+    return undefined;
+  }
+
+  while (current.next !== null) {
+    current = current.next;
+    miniStack.push(current.value);
+  }
+
+  if (index + 1 > miniStack.length) {
+    return undefined;
+  }
+
+  for (let i = 0; i < index; i++) {
+    miniStack.pop();
+  }
+
+  return miniStack.pop() || undefined;
 };
 
 export default findKthToLast;
