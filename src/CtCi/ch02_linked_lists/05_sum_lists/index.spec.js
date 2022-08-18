ab084e9236abdef425787be755ffb7dd5fc3ccc0
mercuriver <mercuriver@gmail.com>
2022-08-18 11:51:10 +0900

    test(ch02): 05, sumList 테스트 케이스 수정 (이전 케이스는 문제 오독)

--- src/CtCi/ch02_linked_lists/05_sum_lists/index.spec.js
+++ src/CtCi/ch02_linked_lists/05_sum_lists/index.spec.js
@@ -4,41 +4,34 @@ import { LinkedList } from "../utils/index.js";
 
 const data = [
   {
-    list: [],
-    expect: 0,
-    expectReverse: 0,
+    firstList: [5],
+    secondList: [9],
+    expectForword: [1, 4],
+    expectReverse: [4, 1],
   },
   {
-    list: [1],
-    expect: 1,
-    expectReverse: 1,
+    firstList: [5, 4],
+    secondList: [7, 9],
+    expectForword: [1, 3, 3],
+    expectReverse: [2, 4, 1],
   },
   {
-    list: [2, 1],
-    expect: 21,
-    expectReverse: 12,
-  },
-  {
-    list: [3, 5, 8, 5, 1, 2, 1],
-    expect: 3585121,
-    expectReverse: 1215853,
-  },
-  {
-    list: [9, 8, 7, 6, 5, 4, 3, 2, 1],
-    expect: 987654321,
-    expectReverse: 123456789,
+    firstList: [7, 1, 6],
+    secondList: [5, 9, 2],
+    expectForword: [1, 3, 0, 8],
+    expectReverse: [2, 1, 9],
   },
 ];
 
 describe("리스트의 합", function () {
-  data.forEach((datum) => {
-    it(`[정방향] ${datum.list} 의 합 ${datum.expect}`, function () {
-      expect(sumList(new LinkedList(datum.list)), true).to.equal(datum.expect);
-    });
-  });
-  data.forEach((datum) => {
-    it(`[역방향] ${datum.list} 의 합 ${datum.expect}`, function () {
-      expect(sumList(new LinkedList(datum.list))).to.equal(datum.expect);
-    });
-  });
+  // data.forEach((datum) => {
+  //   it(`[정방향] ${datum.list} 의 합 ${datum.expect}`, function () {
+  //     expect(sumList(new LinkedList(datum.list)), true).to.equal(datum.expect);
+  //   });
+  // });
+  // data.forEach((datum) => {
+  //   it(`[역방향] ${datum.list} 의 합 ${datum.expect}`, function () {
+  //     expect(sumList(new LinkedList(datum.list))).to.equal(datum.expect);
+  //   });
+  // });
 });
