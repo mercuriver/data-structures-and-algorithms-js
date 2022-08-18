326dca45ff1e7cb763c2dde2dea2c6d82963aab3
mercuriver <mercuriver@gmail.com>
2022-08-18 11:34:07 +0900

    feat(ch02): ch05, 리스트 -> 정수형, JS 기반 처리

--- src/CtCi/ch02_linked_lists/05_sum_lists/index.js
+++ src/CtCi/ch02_linked_lists/05_sum_lists/index.js
@@ -1,5 +1,22 @@
-const solution = () => {
-  return undefined;
+const sumListJS = (list, isReverse = false) => {
+  const buffer = [];
+
+  let current = list.head;
+
+  while (current !== null) {
+    if (isReverse) {
+      buffer.unshift(current.value);
+    } else {
+      buffer.push(current.value);
+    }
+    current = current.next;
+  }
+
+  return buffer.length <= 0 ? 0 : parseInt(buffer.join(""), 10);
 };
 
-export default solution;
+const sumList = (list, isReverse = false) => {
+  return 0;
+};
+
+export default sumList;
