a247a7b280b23f544f7dba54fda97f1c24e026c1
mercuriver <mercuriver@gmail.com>
2022-08-18 11:05:08 +0900

    feat(ch02): ch04, 분할 문제, 큐 기반 풀이

--- src/CtCi/ch02_linked_lists/04_partition/index.js
+++ src/CtCi/ch02_linked_lists/04_partition/index.js
@@ -1,4 +1,32 @@
 const partition = (list, target) => {
+  const leftQueue = [];
+  const rightQueue = [];
+
+  let current = list.head;
+
+  while (current !== null) {
+    if (current.value < target) {
+      leftQueue.push(current);
+    } else {
+      rightQueue.push(current);
+    }
+
+    current = current.next;
+  }
+
+  if (leftQueue.length <= 0) {
+    return list;
+  }
+
+  let newCurrent = leftQueue.shift();
+  list.head = newCurrent;
+
+  for (const unit of [...leftQueue, ...rightQueue]) {
+    newCurrent.next = unit;
+    newCurrent = unit;
+  }
+  newCurrent.next = null;
+
   return list;
 };
 
