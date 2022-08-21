388439f5b86b9259e5646cd7dc6c757d23c399c1
mercuriver <mercuriver@gmail.com>
2022-08-21 17:48:32 +0900

    refactor(ch02): q05, forward 코드, 변수 정리

--- src/CtCi/ch02_linked_lists/05_sum_lists/forward.js
+++ src/CtCi/ch02_linked_lists/05_sum_lists/forward.js
@@ -2,43 +2,32 @@ import { LinkedList } from "../utils/index.js";
 
 const emptyNode = { value: 0, next: null };
 
-const phasedSum = ({
-  firstNode: currentFirstNode,
-  firstLength: currentFirstLength,
-  secondNode: currentSecondNode,
-  secondLength: currentSecondLength,
-}) => {
-  if (currentFirstLength + currentSecondLength === 0) {
+const phasedSum = (payload) => {
+  const { firstNode, firstLength, secondNode, secondLength } = payload;
+  if (firstLength + secondLength === 0) {
     return { carry: 0, value: [] };
   }
 
-  let nextFirstNode = currentFirstNode;
-  let nextFirstLength = currentFirstLength;
-  let nextSecondNode = currentSecondNode;
-  let nextSecondLength = currentSecondLength;
+  const nextPayload = { ...payload };
 
   let firstValue = 0;
   let secondValue = 0;
 
-  if (currentFirstLength >= currentSecondLength) {
-    const { value, next } = currentFirstNode || emptyNode;
+  if (firstLength >= secondLength) {
+    const { value, next } = firstNode || emptyNode;
     firstValue = value;
-    nextFirstNode = next;
-    nextFirstLength = currentFirstLength - 1;
-  }
-  if (currentSecondLength >= currentFirstLength) {
-    const { value, next } = currentSecondNode || emptyNode;
-    secondValue = value;
-    nextSecondNode = next;
-    nextSecondLength = currentSecondLength - 1;
+    nextPayload.firstNode = next;
+    nextPayload.firstLength = firstLength - 1;
   }
 
-  const { carry, value } = phasedSum({
-    firstNode: nextFirstNode,
-    firstLength: nextFirstLength,
-    secondNode: nextSecondNode,
-    secondLength: nextSecondLength,
-  });
+  if (secondLength >= firstLength) {
+    const { value, next } = secondNode || emptyNode;
+    secondValue = value;
+    nextPayload.secondNode = next;
+    nextPayload.secondLength = secondLength - 1;
+  }
+
+  const { carry, value } = phasedSum(nextPayload);
 
   const currentSumValue = firstValue + secondValue + carry;
   const currentCarry = currentSumValue >= 10 ? 1 : 0;
