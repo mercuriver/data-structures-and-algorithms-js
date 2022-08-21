502c8b972aaa8f88314935163186a5ebdf4a9629
mercuriver <mercuriver@gmail.com>
2022-08-21 18:01:51 +0900

    feat(ch02): q06, length 값을 사용하지 않은 문자열 반전 풀이

--- src/CtCi/ch02_linked_lists/06_palindrome/index.js
+++ src/CtCi/ch02_linked_lists/06_palindrome/index.js
@@ -1,5 +1,31 @@
+import { LinkedList } from "../utils/index.js";
+
+const reverse = (node) => {
+  if (node === null) {
+    return [];
+  }
+
+  const { value, next } = node || { value: "", next: null };
+  return [...reverse(next), value];
+};
+
 const isPalindrome = (list) => {
-  return false;
+  // Linked list의 length 값을 알 수 없을 경우의 풀이
+  const reversed = new LinkedList(reverse(list.head));
+
+  let current = list.head;
+  let reversedCurrent = reversed.head;
+  let checkIsPalindrome = true;
+
+  while (current.next && checkIsPalindrome) {
+    if (current.value !== reversedCurrent.value) {
+      checkIsPalindrome = false;
+    }
+    current = current.next;
+    reversedCurrent = reversedCurrent.next;
+  }
+
+  return checkIsPalindrome;
 };
 
 export default isPalindrome;
