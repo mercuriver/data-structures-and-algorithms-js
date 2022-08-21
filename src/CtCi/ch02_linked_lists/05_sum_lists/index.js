f5398ccb6a0482b928d527d930e93b472de446f3
mercuriver <mercuriver@gmail.com>
2022-08-21 11:31:40 +0900

    feat(ch02): q05, 재귀적 풀이 구현

--- src/CtCi/ch02_linked_lists/05_sum_lists/index.js
+++ src/CtCi/ch02_linked_lists/05_sum_lists/index.js
@@ -1,9 +1,28 @@
+import { LinkedList } from "../utils/index.js";
+
 const sumListForward = (firstList, secondList) => {
   return firstList;
 };
 
-const sumList = (firstList, secondList) => {
-  return firstList;
+const emptyNode = { value: 0, next: null };
+
+const phasedSum = (firstNode, secondNode, carry) => {
+  const { value: firstValue, next: firstNext } = firstNode || emptyNode;
+  const { value: secondValue, next: secondNext } = secondNode || emptyNode;
+
+  const sumValue = firstValue + secondValue + carry;
+
+  if (sumValue === 0) {
+    return [];
+  }
+
+  const currentCarry = sumValue > 10 ? 1 : 0;
+  const currentValue = sumValue % 10;
+
+  return [currentValue, ...phasedSum(firstNext, secondNext, currentCarry)];
 };
+
+const sumList = (firstList, secondList) =>
+  new LinkedList(phasedSum(firstList.head, secondList.head, 0));
 
 export { sumList, sumListForward };
