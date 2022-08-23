8b8e040e5d6cda6b5ed7ae9cdf3973a84397ecdf
Mercuriver <mercuriver@gmail.com>
2022-08-23 22:10:28 +0900

    refactor(ch02): 미사용 utils 제거

--- src/CtCi/ch02_linked_lists/utils/index.js
+++ src/CtCi/ch02_linked_lists/utils/index.js
@@ -1,24 +1,4 @@
 export { default as Node } from "./Node.js";
 export { default as LinkedList } from "./LinkedList.js";
 
-const createNode = (node, next) => ({
-  node,
-  next: next || null,
-});
 
-const arrayToLinkedList = (arrayValue) => {
-  const { length } = arrayValue || { length: 0 };
-  let linkedList = null;
-
-  if (length <= 0) {
-    return linkedList;
-  }
-
-  for (const unit of arrayValue.reverse()) {
-    linkedList = createNode(unit, linkedList);
-  }
-
-  return linkedList;
-};
-
-export { createNode, arrayToLinkedList };
--- src/CtCi/ch02_linked_lists/utils/index.spec.js
+++ src/CtCi/ch02_linked_lists/utils/index.spec.js
@@ -1,42 +1,5 @@
 import { expect } from "chai";
-import { arrayToLinkedList } from "./index.js";
 
-const data = [
-  {
-    list: [1],
-    expect: {
-      node: 1,
-      next: null,
-    },
-  },
-  {
-    list: [1, 2, 3, 4],
-    expect: {
-      node: 1,
-      next: {
-        node: 2,
-        next: {
-          node: 3,
-          next: {
-            node: 4,
-            next: null,
-          },
-        },
-      },
-    },
-  },
-  {
-    list: [],
-    expect: null,
-  },
-];
 
-describe("arrayToLinkedList 테스트", function () {
-  data.forEach((datum) => {
-    it(`${JSON.stringify(datum.list)}의 연결 리스트 데이터: ${JSON.stringify(
-      datum.expect
-    )}`, function () {
-      expect(arrayToLinkedList(datum.list)).to.eql(datum.expect); // passes
-    });
   });
 });
