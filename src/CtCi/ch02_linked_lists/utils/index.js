a86597f0dc07a1527eb82aefc9bea5715cec627d
Mercuriver <mercuriver@gmail.com>
2022-08-23 22:10:44 +0900

    feat(ch02): utils/nodeArrayStringify 추가

--- src/CtCi/ch02_linked_lists/utils/index.js
+++ src/CtCi/ch02_linked_lists/utils/index.js
@@ -1,4 +1,6 @@
 export { default as Node } from "./Node.js";
 export { default as LinkedList } from "./LinkedList.js";
 
+const nodeArrayStringify = (list) => list.map((row) => row.toString());
 
+export { nodeArrayStringify };
--- src/CtCi/ch02_linked_lists/utils/index.spec.js
+++ src/CtCi/ch02_linked_lists/utils/index.spec.js
@@ -1,5 +1,19 @@
 import { expect } from "chai";
+import { Node, nodeArrayStringify } from "./index.js";
 
+const source = ["1-1", "1-2", "1-3"];
 
+describe("nodeArrayStringify 테스트", function () {
+  it(`빈 배열 nodeArrayStringify 테스트`, function () {
+    expect(nodeArrayStringify([])).to.eql([]);
+  });
+
+  const list = source.map((row) => new Node(row));
+  it(`${list} nodeArrayStringify 테스트`, function () {
+    expect(nodeArrayStringify(list)).to.eql([
+      new Node(source[0]).toString(),
+      new Node(source[1]).toString(),
+      new Node(source[2]).toString(),
+    ]);
   });
 });
