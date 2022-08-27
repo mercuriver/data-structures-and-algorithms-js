c47181d222832fa84905d16ae7b8a89a27b499a6
mercuriver <mercuriver@gmail.com>
2022-08-27 23:29:34 +0900

    feat(ch02): q08: fast runner 방식 풀이 구현

--- src/CtCi/ch02_linked_lists/08_loop_detection/runnerLoopNodeDetection.js
+++ src/CtCi/ch02_linked_lists/08_loop_detection/runnerLoopNodeDetection.js
@@ -1,5 +1,29 @@
+const isValidNodeWithNext = (node) => node && node.next !== null;
+
 const loopDetection = (list) => {
-  return undefined;
+  let fastPointer = list.head;
+  let slowPointer = list.head;
+
+  while (isValidNodeWithNext(fastPointer)) {
+    slowPointer = slowPointer.next;
+    fastPointer = fastPointer.next.next;
+
+    if (fastPointer === slowPointer) {
+      break;
+    }
+  }
+
+  if (!isValidNodeWithNext(fastPointer)) {
+    return undefined;
+  }
+
+  slowPointer = list.head;
+  while (fastPointer !== slowPointer) {
+    slowPointer = slowPointer.next;
+    fastPointer = fastPointer.next;
+  }
+
+  return fastPointer;
 };
 
 export default loopDetection;
