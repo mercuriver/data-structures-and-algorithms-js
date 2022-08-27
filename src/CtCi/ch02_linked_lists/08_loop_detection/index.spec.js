268b9bbbc2c2b81a0c0af7da20fc1385e535f05d
mercuriver <mercuriver@gmail.com>
2022-08-27 15:02:40 +0900

    test(ch02): q08 테스트 케이스 정리

--- src/CtCi/ch02_linked_lists/08_loop_detection/index.spec.js
+++ src/CtCi/ch02_linked_lists/08_loop_detection/index.spec.js
@@ -1,5 +1,5 @@
 import { expect } from "chai";
-import { LinkedList, nodeArrayStringify } from "../utils/index.js";
+import { LinkedList, nodeArrayStringify, skipNode } from "../utils/index.js";
 import loopDetection from "./index.js";
 
 const source = ["1-1", "1-2", "1-3", "1-4", "1-5", "1-6"];
@@ -32,32 +32,51 @@ describe("루프 탐색기", function () {
 describe("루프 탐색기", function () {
   const list = new LinkedList(source);
   list.tail.next = list.tail;
+  const expectList = new LinkedList(source);
   it(`'${source[source.length - 1]}' tail 반복`, function () {
-    expect(nodeArrayStringify(loopDetection(list))).to.be.eql(
-      nodeArrayStringify([list.tail])
+    expect(nodeArrayStringify(loopDetection(list), true)).to.be.eql(
+      nodeArrayStringify([expectList.tail], true)
     );
   });
 });
 
 describe("루프 탐색기", function () {
   const list = new LinkedList(source);
-  const head = list.head;
-  const expectList = [head.next, head.next.next, head.next.next.next];
-  head.next.next.next.next = head.next;
+  skipNode(list.head, 3).next = skipNode(list.head, 1);
+
+  const expectHead = new LinkedList(source).head;
+  const expectList = [
+    skipNode(expectHead, 1),
+    skipNode(expectHead, 2),
+    skipNode(expectHead, 3),
+  ];
 
   it(`'${[source[1], source[2], source[3]]}' 부분 반복`, function () {
-    expect(nodeArrayStringify(loopDetection(list))).to.be.eql(
-      nodeArrayStringify(expectList)
+    expect(nodeArrayStringify(loopDetection(list), true)).to.be.eql(
+      nodeArrayStringify(expectList, true)
     );
   });
 });
 
-// describe("루프 탐색기", function () {
-//   const list = new LinkedList(source);
-//   const [, , , ...rest] = source;
-//   const expectList = new LinkedList(rest);
-//   list.tail.next = list.head.next.next.next;
-//   it(`'${source}' tail 반복`, function () {
-//     expect(loopDetection(list).toArray()).to.be.eql(expectList);
-//   });
-// });
+describe("루프 탐색기", function () {
+  const { length } = source;
+  const list = new LinkedList(source);
+  list.tail.next = skipNode(list.head, length - 3);
+
+  const expectHead = new LinkedList(source).head;
+  const expectList = [
+    skipNode(expectHead, length - 3),
+    skipNode(expectHead, length - 2),
+    skipNode(expectHead, length - 1),
+  ];
+
+  it(`'${[
+    source[length - 1],
+    source[length - 3],
+    source[length - 2],
+  ]}' tail 부터 부분 반복`, function () {
+    expect(nodeArrayStringify(loopDetection(list), true)).to.be.eql(
+      nodeArrayStringify(expectList, true)
+    );
+  });
+});
