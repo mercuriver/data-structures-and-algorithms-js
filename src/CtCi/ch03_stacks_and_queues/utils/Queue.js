0f0c18cf936a8068f8e9b4d56ebcfda29795765a
mercuriver <mercuriver@gmail.com>
2022-08-28 16:50:33 +0900

    feat(ch03): class Queue 작업 #1

---
+++ src/CtCi/ch03_stacks_and_queues/utils/Queue.js
@@ -0,0 +1,20 @@
+class Queue {
+  #list;
+  constructor() {
+    this.#list = [];
+  }
+
+  enqueue(value) {}
+
+  dequeue() {}
+
+  peek() {}
+
+  poll() {}
+
+  isEmpty() {}
+
+  toArray() {}
+}
+
+export default Queue;
